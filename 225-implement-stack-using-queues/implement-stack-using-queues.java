9263daceb1bf32f1a18c6f87c929cb5622f93476
Reena-prabin <reenaprabin06@gmail.com>
2026-09-15 04:49:31 +0530

    Time: 2 ms (28.66%) | Memory: 42.8 MB (53.88%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -0,0 +1,19 @@
+class MyStack {
+    Queue<Integer> q = new LinkedList<>();
+    public void push(int x) {
+        q.add(x);
+        int n = q.size();
+        for (int i = 1; i < n; i++) {
+            q.add(q.remove());
+        }
+    }
+    public int pop() {
+        return q.remove();
+    }
+    public int top() {
+        return q.peek();
+    }
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
